3fd83d8b288e98966747ca8839daaa6a2188a462
박근채 <pgc0419@gmail.com>
2025-01-11 15:45:07 +0900

    design: 성공 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChoosePage from "./pages/ChoosePage";
 import FailPage from "./pages/FailPage";
 import GiveUpPage from "./pages/GiveUpPage";
+import SuccessPage from "./pages/SuccessPage";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path="/choose" element={<ChoosePage />} />
         <Route path="/fail" element={<FailPage />} />
         <Route path="/give-up" element={<GiveUpPage />} />
+        <Route path="/Success" element={<SuccessPage />} />
       </Routes>
     </Router>
   );
