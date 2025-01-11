d70ec63d7eba9be4f81c80305524f857c9fc2a06
박근채 <pgc0419@gmail.com>
2025-01-11 17:46:39 +0900

    design: 성공 페이지 라우팅

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const SuccessPage: React.FC = () =>{
+const SuccessPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  // 배경 클릭 핸들러
+  const handleBackgroundClick = () => {
+    navigate("/ending"); // "/ending" 경로로 이동
+  };
+
   return (
     <div
-    className="fixed inset-0 bg-cover bg-center bg-no-repeat"
-    style={{
-    backgroundImage: 'url(/images/SuccessPage.png)',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    backgroundBlendMode: 'multiply',
-    }}
+      className="fixed inset-0 bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: 'url(/images/SuccessPage.png)',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundBlendMode: 'multiply',
+      }}
+      onClick={handleBackgroundClick} // 배경 클릭 이벤트 등록
     >
-
       <div className="absolute bottom-1/2 left-1/2 transform -translate-x-1/2 text-center">
-        <p className="font-Binggrae font-bold drop-shadow-md"
-        style={{
+        <p
+          className="font-Binggrae font-bold drop-shadow-md"
+          style={{
             fontSize: "80px",
-            color: "#FFD700"
-        }}
+            color: "#FFD700",
+          }}
         >
           Congratulations
         </p>
