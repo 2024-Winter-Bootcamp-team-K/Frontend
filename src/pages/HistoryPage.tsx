20b2d726699cd840d0172c019e61880978ec8a69
suyeon <suyeon040102@gmail.com>
2025-01-15 20:49:33 +0900

    design: 시나리오 생성 페이지 디자인 개선

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -20,7 +20,7 @@ const HistoryPage = () => {
       setTimeout(() => setShowImage(true), 0);
     }, 0);
     return () => clearTimeout(timer);
-  }, []);
+  }, [popupText]);
 
   const handleBackgroundClick = (e: React.MouseEvent) => {
     const target = e.target as HTMLElement;
