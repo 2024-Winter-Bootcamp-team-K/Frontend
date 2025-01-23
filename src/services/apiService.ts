607cacb1bd4bb20dd9a11eca218d13f1ec78e568
박근채 <pgc0419@gmail.com>
2025-01-24 01:52:30 +0900

    feat: 시나리오 로딩 구현 완료

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -17,26 +17,24 @@ export const createScenario = async (data: unknown) => {
   }
 };
 
-// 증거 생성 함수
-export const createEvidence = async (scenarioId: string) => {
-try {
-  const response = await axios.post(`${API_BASE_URL}/scenarios/evidence/${scenarioId}`);
-  return response.data;
-} catch (error) {
-  console.error("시나리오 생성 중 오류 발생:", error);
-  throw error;
-}
+export const createEvidence = async (scenario_id: number) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/scenarios/evidence`, { scenario_id });
+    return response.data;
+  } catch (error) {
+    console.error("증거 생성 중 오류 발생:", error);
+    throw error;
+  }
 };
 
-// 용의자 생성 함수
-export const createSuspect = async (scenarioId: string) => {
-try {
-  const response = await axios.post(`${API_BASE_URL}/scenarios/suspect/${scenarioId}`);
-  return response.data;
-} catch (error) {
-  console.error("시나리오 생성 중 오류 발생:", error);
-  throw error;
-}
+export const createSuspect = async (scenario_id: number) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/scenarios/suspect`, { scenario_id });
+    return response.data;
+  } catch (error) {
+    console.error("용의자 생성 중 오류 발생:", error);
+    throw error;
+  }
 };
 
 // 시나리오 조회 함수
