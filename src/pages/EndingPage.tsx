61856d723f5c04d5dbe6d5d6af84c01768c2f42b
박근채 <pgc0419@gmail.com>
2025-01-30 22:43:30 +0900

    fix: lena 멘토님 이름 오타 수정

--- src/pages/EndingPage.tsx
+++ src/pages/EndingPage.tsx
@@ -178,7 +178,7 @@ const EndingPage: React.FC = () => {
           <p className="title2">Thanks For</p>
           <p>Andrew Park</p>
           <p>Ryan</p>
-          <p>Rena</p>
+          <p>Lena</p>
           <p>Mindy</p>
           <p>Justin</p>
           <p>Sean</p>
